2a4695317476f6851a529995c1d113a3acdca3f9
Azzerty23 <azzerty23@hotmail.fr>
2019-11-04 12:53:28 +0100

    create Attendance model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :administrator, class_name: "User"
+  has_many :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :administrator_events, foreign_key: 'administrator_id', class_name: "Event"
+  has_many :attendances
 end
