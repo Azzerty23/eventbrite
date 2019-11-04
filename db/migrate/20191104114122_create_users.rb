7f9aff41a2f84ff3b0a8e6445c68256092652a9e
Azzerty23 <azzerty23@hotmail.fr>
2019-11-04 13:52:15 +0100

    typo : encrypted_password

--- db/migrate/20191104114122_create_users.rb
+++ db/migrate/20191104114122_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email, null: false, default: ""
-      t.string :encypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
       t.text :description
       t.string :firstname
       t.string :lastname
