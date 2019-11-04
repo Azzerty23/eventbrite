b603b5a2152b05105cf0816b41d7e7a5afba7453
Azzerty23 <azzerty23@hotmail.fr>
2019-11-04 14:09:54 +0100

    add users foreign_key constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :administrator_events, foreign_key: 'administrator_id', class_name: "Event"
+  has_many :administrator_events, foreign_key: 'administrator_id', class_name: "Event", dependent: :destroy
   has_many :attendances
 end
