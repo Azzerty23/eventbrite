b4c97c54888d351965981a0c7b3da4c6a2f9e05f
Azzerty23 <azzerty23@hotmail.fr>
2019-11-05 21:59:01 +0100

    add attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,16 +1,39 @@
 class AttendancesController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :create]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
-  def index
-    @attendances = Attendance.where(user_id: current_user)
-  end
+  before_action :set_user
+  before_action :set_attendance, only: [:destroy]
 
-  def new
-    @attendance = Attendance.new
-  end
-
+  # POST /attendances
   def create
-    
+    @attendance = Attendance.new(attendance_params)
+    if @attendance.save
+      flash[:success] = "Votre participation sera notifié à l'organisateur"
+    else
+      flash[:warning] = "Vous participation n'a pas pu être sauvegarder. Contacter l'organisateur pour de plus amples informations"
+    end
+      
+    redirect_back(fallback_location: attendances_path)
   end
+
+  # DELETE /attendances/1
+  def destroy
+    @attendance.destroy
+    flash[:danger] = 'Votre participation a été annulée'
+    redirect_back(fallback_location: attendances_path)
+  end
+
+  private
+    def set_attendance
+      @attendance = Attendance.find(params[:id])
+    end
+
+    def set_user
+      @user = User.find(current_user.id)
+    end
+
+    def attendance_params
+      params.require(:attendance).permit(:event_id, :user_id)
+    end
 
 end
