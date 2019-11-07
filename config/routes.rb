fc43a8723f0d1650576ab2090c900498e02985f6
mariollet <mariollet@outlook.fr>
2019-11-07 19:17:10 +0100

    test Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   
   root to: "static_pages#index"
-  # get 'static_pages/index'
   get 'static_pages/secret'
   get "team", to: "static_pages#team"
   get "contact", to: "static_pages#contact"
   get "user", to: "static_pages#show"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   resources :events
   resources :attendances do
     resources :charges
